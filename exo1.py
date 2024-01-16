1c71b5e98d32f606bc44e6d1aee55a4b30e7bed6
BastienLAFONT <148950652+BastienLAFONT@users.noreply.github.com>
2024-01-16 10:47:30 +0100

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -1,14 +1,43 @@
-class Robot():
-    __name = "<unknown>"
-    __power = False
-    __current_speed = 0
-    __battery_level = 0
-    __states = ['shutown', 'running']
-    
-        
-    def SetName(self, name):
-        self.__name=name
+import time
 
+class Robot:
+    def __init__(self, nom):
+        self.nom = nom
+        self.allume = False
+        self.charge = 0
+        self.vitesse = 0
+
+    def allumer(self):
+        if not self.allume:
+            self.allume = True
+            print(f"{self.nom} est allumé.")
+
+    def eteindre(self):
+        if self.allume:
+            self.allume = False
+            print(f"{self.nom} est éteint.")
+
+    def charger_batterie(self):
+        if self.allume:
+            for _ in range(11):
+                time.sleep(1)
+                self.charge = _ * 10
+            self.charge = 100
+
+    def enregistrer_vitesse(self, vitesse):
+        self.vitesse = vitesse
+
+    def obtenir_vitesse(self):
+        return self.vitesse
+
+    def arreter_deplacement(self):
+        if self.vitesse > 0:
+            self.vitesse = 0
+            
+
+    def resume_etat(self):
+        etat = "allumé" if self.allume else "éteint"
+        return f"{self.nom} est {etat} avec une charge de {self.charge}%, se déplace à {self.vitesse} km/h."
 
 
 
